4c7157a29e4b4d301e3221d315568970c81acee0
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-02-04 20:56:41 -0500

    Household_address documentation

--- app/Http/Controllers/HouseholdAddressController.php
+++ app/Http/Controllers/HouseholdAddressController.php
@@ -17,27 +17,27 @@ class HouseholdAddressController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
+     *
+     * @apiDescription
+     * Adds a household address
+     *
+     * @api {POST} /api/household_address/ Add Address
+     * @apiName Add Address
+     * @apiGroup Household Address
+     * @apiVersion 1.0.0
+     * @apiParam {string} type
+     * @apiParam {string} address_street
+     * @apiParam {string} [address_street2]
+     * @apiParam {string} address_city
+     * @apiParam {string} address_state
+     * @apiParam {int[5]} address_zip
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *   "id": 1
+     * }
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -50,7 +50,28 @@ class HouseholdAddressController extends Controller
 
     /**
      * Display the specified resource.
+     * @apiDescription
+     * Returns a household address
      *
+     * @api {GET} /api/household_address/:id Show Address
+     * @apiName Show Address
+     * @apiGroup Household Address
+     * @apiVersion 1.0.0
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *   "id": 1,
+     *   "created_at": "2016-02-03 01:53:41",
+     *   "updated_at": "2016-02-03 01:53:41",
+     *   "household_id": 4,
+     *   "type": "Work",
+     *   "address_street": "116 Koss Springs Apt. 567",
+     *   "address_street2": "",
+     *   "address_city": "Deanbury",
+     *   "address_state": "OK",
+     *   "address_zip": "42864"
+     * }
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
@@ -60,19 +81,26 @@ class HouseholdAddressController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
+     * @apiDescription
+     * Updates a household address
      *
+     * @api {PUT} /api/household_address/:id Update Address
+     * @apiName Update Address
+     * @apiGroup Household Address
+     * @apiVersion 1.0.0
+     * @apiParam {string} type
+     * @apiParam {string} address_street
+     * @apiParam {string} [address_street2]
+     * @apiParam {string} address_city
+     * @apiParam {string} address_state
+     * @apiParam {int[5]} address_zip
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *   "id": 1
+     * }
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -85,16 +113,5 @@ class HouseholdAddressController extends Controller
         } catch (Exception $e) {
 
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
