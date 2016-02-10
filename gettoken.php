3fdbbacb2f56df11e1d0068b34982625dd0ad719
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-02-09 20:13:44 -0500

    PHP command line tool (gettoken.php) for generating auth tokens

---
+++ gettoken.php
@@ -0,0 +1,42 @@
+<?php
+if(empty($argv[1])) {
+    $argv[1] = "dev";
+}
+
+switch ($argv[1]) {
+    case "staging":
+        $url = "http://wintergift-api.codeforcharlotte.org/api/login";
+        break;
+
+    case "dev":
+    case "local":
+    case "development":
+    default:
+        if (gethostname() == "cmpd-holiday-gift-backend") {
+            $url = "http://localhost/api/login";
+        } else {
+            $url = "http://homestead.app/api/login";
+        }
+        break;
+
+    case "help":
+        die("\nUsage: php gettoken.php development|staging\n\n");
+        break;
+}
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+    "email=developer@codeforcharlotte.org&password=admin");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$resp = curl_exec ($ch);
+curl_close ($ch);
+
+try {
+    $resp = json_decode($resp);
+    echo "\n\nToken: \n{$resp->token}\n\n";
+} catch (Exception $e) {
+    die("Nope. Something's not right :(");
+}
