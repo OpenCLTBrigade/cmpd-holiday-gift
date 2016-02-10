48e836d61e53ee98956832089c7d7a22709818e8
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-02-09 21:27:41 -0500

    Household documentation

--- app/Http/Controllers/HouseholdController.php
+++ app/Http/Controllers/HouseholdController.php
@@ -17,8 +17,91 @@ class HouseholdController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
+     * @apiDescription Lists all households with their associated `child`, `household_address`, and
+     * `household_phone` records.
      *
+     * @api {GET} /api/household List Households
+     * @apiName List Households
+     * @apiGroup Household
+     * @apiPermission user
+     * @apiVersion 1.0.0
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     * * [
+     *    {
+     *        id: 1,
+     *        nominator_user_id: 1,
+     *        name_first: "Santino",
+     *        name_middle: "Nolan",
+     *        name_last: "Connelly",
+     *        dob: "1985-05-01",
+     *        race: "race",
+     *        gender: "F",
+     *        email: "zThompson@hotmail.com",
+     *        last4ssn: 1082,
+     *        preferred_contact_method: "phone",
+     *        created_at: "2016-02-03 01:53:41",
+     *        updated_at: "2016-02-03 01:53:41",
+     *        child: [
+     *            {
+     *                id: 7,
+     *                created_at: "2016-02-03 01:53:41",
+     *                updated_at: "2016-02-03 01:53:41",
+     *                household_id: 1,
+     *                name_first: "Lilyan",
+     *                name_middle: "Schuyler",
+     *                name_last: "Halvorson",
+     *                dob: "1983-05-16",
+     *                race: "race",
+     *                last4ssn: 7496,
+     *                free_or_reduced_lunch: "Y",
+     *                reason_for_nomination: "Illo sed et minima necessitatibus cum officiis nisi.",
+     *                school_name: "Dicta et non quam magnam qui.",
+     *                school_address: "73214 River Knoll Apt. 387 South Marcelino, AK 83905",
+     *                school_address2: "",
+     *                school_city: "South Solon",
+     *                school_state: "WW",
+     *                school_zip: "79039",
+     *                school_phone: "(234)181-8776x252",
+     *                bike_want: "N",
+     *                bike_size: 12,
+     *                bike_style: "B",
+     *                clothes_want: "Y",
+     *                clothes_size_shirt: "L",
+     *                clothes_size_pants: "M",
+     *                shoe_size: "4",
+     *                favourite_colour: "Red",
+     *                interests: "Quos vel quo ab itaque.",
+     *                additional_ideas: "Aut et et ab commodi accusamus."
+     *            }
+     *        ],
+     *        address: [
+     *            {
+     *                id: 7,
+     *                created_at: "2016-02-03 01:53:41",
+     *                updated_at: "2016-02-03 01:53:41",
+     *                household_id: 1,
+     *                type: "Home",
+     *                address_street: "612 King Valleys",
+     *                address_street2: "Apt 731",
+     *                address_city: "North Juanatown",
+     *                address_state: "OK",
+     *                address_zip: "10443"
+     *            }
+     *        ],
+     *        phone: [
+     *            {
+     *                id: 2,
+     *                created_at: "2016-02-03 01:53:41",
+     *                updated_at: "2016-02-03 01:53:41",
+     *                household_id: 1,
+     *                phone_type: "other",
+     *                phone_number: "(076)328-5606"
+     *            }
+     *        ]
+     *    }
+     *  ]
      * @return \Illuminate\Http\Response
      */
     public function index()
@@ -37,7 +120,30 @@ class HouseholdController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @apiDescription Creates a household record. The `id` of the newly created record is returned so it
+     * can be used to create associated `household_phone`, `household_address`, and `child` records.
+     *
+     * @api {POST} /api/household/ Create household
+     * @apiName Create household
+     * @apiGroup Household
+     * @apiPermission user
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {string} name_first
+     * @apiParam {string} name_middle
+     * @apiParam {string} name_last
+     * @apiParam {date} dob
+     * @apiParam {string} race
+     * @apiParam {string} gender
+     * @apiParam {string} email
+     * @apiParam {int[4]} last4ssn
+     * @apiParam {string} preferred_content_method
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     *    {
+     *      id: 42
+     *    }
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -49,7 +155,89 @@ class HouseholdController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * @apiDescription Returns the requested household with its associated `child`, `household_address`, and
+     * `household_phone` records.
+     *
+     * @api {GET} /api/household/:id Get household
+     * @apiName Get household
+     * @apiGroup Household
+     * @apiPermission user
+     * @apiVersion 1.0.0
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     *    {
+     *        id: 1,
+     *        nominator_user_id: 1,
+     *        name_first: "Santino",
+     *        name_middle: "Nolan",
+     *        name_last: "Connelly",
+     *        dob: "1985-05-01",
+     *        race: "race",
+     *        gender: "F",
+     *        email: "zThompson@hotmail.com",
+     *        last4ssn: 1082,
+     *        preferred_contact_method: "phone",
+     *        created_at: "2016-02-03 01:53:41",
+     *        updated_at: "2016-02-03 01:53:41",
+     *        child: [
+     *            {
+     *                id: 7,
+     *                created_at: "2016-02-03 01:53:41",
+     *                updated_at: "2016-02-03 01:53:41",
+     *                household_id: 1,
+     *                name_first: "Lilyan",
+     *                name_middle: "Schuyler",
+     *                name_last: "Halvorson",
+     *                dob: "1983-05-16",
+     *                race: "race",
+     *                last4ssn: 7496,
+     *                free_or_reduced_lunch: "Y",
+     *                reason_for_nomination: "Illo sed et minima necessitatibus cum officiis nisi.",
+     *                school_name: "Dicta et non quam magnam qui.",
+     *                school_address: "73214 River Knoll Apt. 387 South Marcelino, AK 83905",
+     *                school_address2: "",
+     *                school_city: "South Solon",
+     *                school_state: "WW",
+     *                school_zip: "79039",
+     *                school_phone: "(234)181-8776x252",
+     *                bike_want: "N",
+     *                bike_size: 12,
+     *                bike_style: "B",
+     *                clothes_want: "Y",
+     *                clothes_size_shirt: "L",
+     *                clothes_size_pants: "M",
+     *                shoe_size: "4",
+     *                favourite_colour: "Red",
+     *                interests: "Quos vel quo ab itaque.",
+     *                additional_ideas: "Aut et et ab commodi accusamus."
+     *            }
+     *        ],
+     *        address: [
+     *            {
+     *                id: 7,
+     *                created_at: "2016-02-03 01:53:41",
+     *                updated_at: "2016-02-03 01:53:41",
+     *                household_id: 1,
+     *                type: "Home",
+     *                address_street: "612 King Valleys",
+     *                address_street2: "Apt 731",
+     *                address_city: "North Juanatown",
+     *                address_state: "OK",
+     *                address_zip: "10443"
+     *            }
+     *        ],
+     *        phone: [
+     *            {
+     *                id: 2,
+     *                created_at: "2016-02-03 01:53:41",
+     *                updated_at: "2016-02-03 01:53:41",
+     *                household_id: 1,
+     *                phone_type: "other",
+     *                phone_number: "(076)328-5606"
+     *            }
+     *        ]
+     *    }
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -60,18 +248,26 @@ class HouseholdController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * @apiDescription Updates a household record
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * @api {PUT} /api/household/:id Update Household
+     * @apiName Update Household
+     * @apiGroup Household
+     * @apiPermission user
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {string} name_first
+     * @apiParam {string} name_middle
+     * @apiParam {string} name_last
+     * @apiParam {date} dob
+     * @apiParam {string} race
+     * @apiParam {string} gender
+     * @apiParam {string} email
+     * @apiParam {int[4]} last4ssn
+     * @apiParam {string} preferred_content_method
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
