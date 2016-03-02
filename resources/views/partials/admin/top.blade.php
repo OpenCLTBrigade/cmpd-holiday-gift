2da902a10f9fbe715b7c7514cf2e16fd259516db
Alejandro Sosa <Alejandro Sosa>
2016-03-01 21:48:29 -0500

    Changed user photo button in top right (that opens menu) to user's name. Removed photo and language switcher, and leaving user's name and logout button.

--- resources/views/partials/admin/top.blade.php
+++ resources/views/partials/admin/top.blade.php
@@ -6,26 +6,15 @@
         </a>
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img class="user-image img-circle" src="{{ !empty($user->picture) ? $user->picture : 'https://ssl.gstatic.com/accounts/ui/avatar_2x.png' }}" alt="{{ Auth::user()->name  }}" />
-                        <span class="hidden-xs">{{ Auth::user()->name  }}</span>
+                <li class="user">
+					<a href="#" >
+						<span class="hidden-xs">{{ Auth::user()->name_first  }}</span>
                     </a>
-                    <ul class="dropdown-menu">
-                        <li class="user-header">
-                            <img class="img-circle" src="{{ !empty($user->picture) ? $user->picture : 'https://ssl.gstatic.com/accounts/ui/avatar_2x.png' }}" alt="{{ Auth::user()->name  }}" />
-                            <p>{{ Auth::user()->name  }}</p>
-                            @include('partials.common.languages', ['languages' => Config::get('languages'), 'route' => 'admin.language.change' ])
-                        </li>
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="{{ route('root')  }}" class="btn btn-default btn-flat"><i class="fa fa-globe"></i> {{ trans('application.home') }}</a>
-                            </div>
-                            <div class="pull-right">
-                                <a href="{{ route('auth.logout') }}" class="btn btn-default btn-flat"><i class="fa fa-sign-out"></i> {{ trans('auth.logout') }}</a>
-                            </div>
-                        </li>
-                    </ul>
+				</li>
+				<li class="dropdown user user-menu">
+                    <a href="{{ route('auth.logout')}}" >
+						<i class="fa fa-sign-out"></i> {{ trans('auth.logout') }}
+                    </a>
                 </li>
             </ul>
         </div>
