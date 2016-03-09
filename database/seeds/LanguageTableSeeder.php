6229fff49e491f0fb2b4079c212b319b16b47981
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-03-08 21:35:03 -0500

    Update language table seeder to be English + Spanish

--- database/seeds/LanguageTableSeeder.php
+++ database/seeds/LanguageTableSeeder.php
@@ -8,7 +8,7 @@ class LanguageTableSeeder extends Seeder
     public function run()
     {
         DB::table('languages')->delete();
-        Language::create(['title' => 'Türkçe', 'code' => 'tr', 'site_title' => 'Blog', 'site_description' => 'Muhteşem Blogum']);
-        Language::create(['title' => 'English', 'code' => 'en', 'site_title' => 'Blog', 'site_description' => 'My Awesome Blog']);
+        Language::create(['title' => 'Spanish', 'code' => 'sp', 'site_title' => 'CMPD Winter Gift Project', 'site_description' => '']);
+        Language::create(['title' => 'English', 'code' => 'en', 'site_title' => 'CMPD Winter Gift Project', 'site_description' => '']);
     }
 }
