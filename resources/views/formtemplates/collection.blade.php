42ac630c3b138f96a1e1fa570b4293d842c26a36
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-03-25 18:02:11 -0400

    Custom collection form template

---
+++ resources/views/formtemplates/collection.blade.php
@@ -0,0 +1,17 @@
+<?php if ($showField): ?>
+    <?php foreach ((array)$options['children'] as $child): ?>
+        <div class="box">
+            <?php if ($showLabel && $options['label'] !== false): ?>
+            <div class="box-header with-border">
+                <h3 class="box-title">
+                    <?= Form::label($name, $options['label'], $options['label_attr']) ?>
+                </h3>
+            </div>
+            <?php endif; ?>
+
+            <div class="box-body">
+                <?= $child->render() ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php endif; ?>
