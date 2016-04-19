57265c32ffb854e5bc2cc519130ff6615206e69e
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-04-19 19:44:53 -0400

    Update permissions seeder to reflect latest batch of permissions we need outside of only using role definitions

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -12,109 +12,45 @@ class PermissionTableSeeder extends Seeder
      */
     public function run()
     {
+        
         /*
-         * Permissions we have to create
+         * Hey you! Yes, you there! TRY and keep this docblock up to date will ya pease? O:-)
 
-            Admin-specific
-            - Approve account requests
-            - Approve nominations
-            - Can view all nominees.
-            - Reporting features for Salvation Army/Link109
+            LIST OF PERMISSIONS
 
-            Partner-specific
-            - Receive reports
+            - manager-users
+            - manage-nominations - In place of "add-nominations"
+            - add-nominations - In place of "manage-nominations"
+            - access-reports
         */
 
         /* Admin -- Approve and Create an account */
-        $approveAccount = new Permission();
-        $approveAccount->name = "user-create";
-        $approveAccount->display_name = "Create account requests";
-        $approveAccount->description = "Create and approve user accounts.";
-        $approveAccount->save();
+        $manageUsers = new Permission();
+        $manageUsers->name = "manage-users";
+        $manageUsers->display_name = "Manage User Accounts";
+        $manageUsers->description = "Create, edit, and approve users";
+        $manageUsers->save();
 
-        /* Admin -- Edit all users */
-        $editAccount = new Permission();
-        $editAccount->name = "user-edit";
-        $editAccount->display_name = "Edit all users";
-        $editAccount->save();
+        $manageNominations = new Permission();
+        $manageNominations->name = "manage-nominations";
+        $manageNominations->display_name = "Manage All Nominations";
+        $manageNominations->description = "View, create, and edit all nominations";
+        $manageNominations->save();
 
-        /*Admin -- View all users */
-        $viewUserList = new Permission();
-        $viewUserList->name ="user-list";
-        $viewUserList->display_name = "View user list";
-        $viewUserList->save();
+        $addNominations = new Permission();
+        $addNominations->name = "manage-users";
+        $addNominations->display_name = "Add Nominations";
+        $addNominations->description = "Add nominations from school";
+        $addNominations->save();
 
-        /* Admin -- Approve nomination */
-        $approveNomination = new Permission();
-        $approveNomination->name = "nomination-approve";
-        $approveNomination->display_name = "Approve Nominations";
-        $approveNomination->save();
-
-        /* Admin - view all Nominees */
-        $viewNominee = New Permission();
-        $viewNominee->name = "nomination-view-all";
-        $viewNominee->display_name = "View All Nominees";
-        $viewNominee->save();
-
-        /* Admin and Partners -- receive reports */
-        $reporting = New Permission();
-        $reporting->name = "reporting";
-        $reporting->display_name = "Reporting Functions";
-        $reporting->description = "Reporting features for Salvation Army, etc.";
-        $reporting->save();
+        $accessReports = new Permission();
+        $accessReports->name = "access-reports";
+        $accessReports->display_name = "Access Reports";
+        $accessReports->description = "Access reporting features";
+        $accessReports->save();
 
 
-        /*
-         Nominee-specific 
-         - View nomination status
-         - Update Information
-        */
-
-        /* Nominee -- View own status */
-        $viewOwnNominee = new Permission();
-        $viewOwnNominee->name = "nomination-view-own";
-        $viewOwnNominee->display_name = "View own nominees";
-        $viewOwnNominee->save();
-
-        /* Nominee -- Edit own status */
-        $editOwnNomination = new Permission();
-        $editOwnNomination->name = "nomination-edit-own";
-        $editOwnNomination->display_name = "Edit own nomination";
-        $editOwnNomination->save();
-
-
-        /* 
-         Nominator Status --- Still needs Updating 
-
-         Nominator-specific
-            - Can enter nominees
-            - Can see list of all nominees for their school.
-            - Can edit their nominee's information
-            - Can see nominee status
-        */
-
-        /* Nominator -- Enter Nominee */
-        $createNominee = new Permission();
-        $createNominee->name = "nomination-create";
-        $createNominee->display_name = "Enter Nominees";
-        $createNominee->save();
-
-        /* Nominator -- Edit Any Nominee*/
-        $editNomination = new Permission();
-        $editNomination->name = "nomination-edit";
-        $editNomination->display_name = "Edit any Nominee";
-        $editNomination->save();
-
-        $createNomineeCMS = new Permission();
-        $createNomineeCMS->name = "nomination-create-cms";
-        $createNomineeCMS->display_name = "Enter Nominees from own School";
-        $createNomineeCMS->save();
-
-        $viewNomineeForSchool = new Permission();
-        $viewNomineeForSchool->name = "nomination-view-cms";
-        $viewNomineeForSchool->display_name = "View Nominees from own School";
-        $viewNomineeForSchool->save();
-
+        // PERMISSIONS BELOW ARE TBD.
 
         /*
          Volunteers -- May need updating
