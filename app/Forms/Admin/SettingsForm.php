c76ee54994e608f577d3c613bfe5edf564dc7c89
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-05-17 21:17:11 -0400

    Switch up the settings form

--- app/Forms/Admin/SettingsForm.php
+++ app/Forms/Admin/SettingsForm.php
@@ -12,21 +12,8 @@ class SettingsForm extends AdminForm
             ->add('email', 'text', [
                 'label' => trans('admin.fields.setting.email')
             ])
-            ->add('facebook', 'text', [
-                'label' => trans('admin.fields.setting.facebook')
-            ])
-            ->add('twitter', 'text', [
-                'label' => trans('admin.fields.setting.twitter')
-            ])
             ->add('analytics_id', 'text', [
                 'label' => trans('admin.fields.setting.analytics_id')
-            ])
-            ->add('disqus_shortname', 'text', [
-                'label' => trans('admin.fields.setting.disqus_shortname')
-            ])
-            ->add('logo', 'file', [
-                'label' => trans('admin.fields.setting.logo'),
-                'attr' => ['class' => '']
             ]);
         parent::buildForm();
     }
