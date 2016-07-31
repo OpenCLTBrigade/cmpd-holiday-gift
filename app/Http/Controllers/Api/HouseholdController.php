36921176bbf423c317bc8b2f0d97aa27a3e50f50
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-07-31 15:06:17 -0400

    Hide PW from JSON, remove household index endpoint, title change

--- app/Http/Controllers/Api/HouseholdController.php
+++ app/Http/Controllers/Api/HouseholdController.php
@@ -9,12 +9,6 @@ use App\Http\Controllers\Controller;
 
 class HouseholdController extends Controller
 {
-    public function index()
-    {
-        // TODO: Increase limit after testing
-        return \App\Household::with("child", "address", "phone")->paginate(3);
-    }
-
     public function show($id)
     {
         return \App\Household::with("child", "address", "phone")->where("id", "=", $id)->get();
