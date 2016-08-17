0aa14e2b0e7e3a30c9caaf3a794c61d83281f1b1
NandaScott <nanda1123@gmail.com>
2016-08-17 14:42:15 -0400

    Trying to simplify listing in the Show Household view

--- resources/views/admin/households/show.blade.php
+++ resources/views/admin/households/show.blade.php
@@ -21,10 +21,13 @@
           <li class="list-group-item">
             <b>Last 4 SSN</b> <a class="pull-right" href="last4ssnto:{{ $object->last4ssn }}">{{ $object->last4ssn }}</a>
           </li>
+          <li class="list-group-item">
+            <b>Preferred Contact Method</b> <a class="pull-right" href="preferred_contact_methodto:{{ $object->preferred_contact_method }}">{{ $object->preferred_contact_method }}</a>
+          </li>
 				</ul>
         <!-- Loop that will list all known values in the HouseholdController -->
-        <ul class="list-group list-group-unbordered">
             @foreach ($object->address as $address)
+            <ul class="list-group list-group-unbordered">
               <li class="list-group-item">
                 <b>Address Street</b> <a class="pull-right">{{$address->address_street}}</a>
               </li>
@@ -40,6 +43,21 @@
               <li class="list-group-item">
                 <b>Zip</b> <a class="pull-right">{{$address->address_zip}}</a>
               </li>
+            </ul>
+            @endforeach
+          <!-- Loop that will list all known value in the ChildController -->
+            @foreach ($object->child as $child)
+                $html = "<ul class="list-group list-group-unbordered">";
+                $html =   "<li class="list-group-item">
+                $html =     "<b>$child</b> <a class="pull-right">{{$child}}</a>";
+                $html =   "</li>"
+                $html = "</ul>";
+                
+            <!-- <ul class="list-group list-group-unbordered">
+              <li class="list-group-item">
+                <b>First Name</b> <a class="pull-right">{{$child->name_first}}</a>
+              </li>
+            </ul> -->
             @endforeach
 			</div>
 			<div class="box-profile col-md-4"></div>
