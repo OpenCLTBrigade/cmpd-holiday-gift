16c7d71ecfbf0f52714c7c256518d4eb4badb4e6
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-10-18 22:08:16 -0400

    Access control for household attachments

--- app/HouseholdAttachment.php
+++ app/HouseholdAttachment.php
@@ -8,5 +8,14 @@ class HouseholdAttachment extends Model
 {
     // Note: the `path' must be formatted as "user-{user_id}/{md5_contents}_{file_name}"
 
-    protected $fillable = [ "household_id", "path" ];
+  protected $fillable = [ "household_id", "path" ];
+
+  public static function boot() {
+    if (\Auth::user() && !\Auth::user()->hasRole("admin"))
+    {
+      static::addGlobalScope('own_records', function(\Illuminate\Database\Eloquent\Builder $builder) {
+        $builder->where('owner', '=', \Auth::user()->id);
+      });
+    }
+  }
 }
