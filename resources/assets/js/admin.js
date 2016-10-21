37950444f411a2b0fa9f7ecd54f6b7618d75ffb4
Braunson Yager <Braunson@users.noreply.github.com>
2016-10-21 16:53:08 -0400

    Update admin.js

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -90,7 +90,7 @@ var DataTable = function (table, details) {
         var result = {
             orderClasses: false,
             fixedHeader: true,
-            responsive: 21,
+            responsive: false,
             dom: "tip",
             stateSave: true,
             stateSaveCallback: function (settings, data) {
