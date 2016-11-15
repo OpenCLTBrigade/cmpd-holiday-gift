99d580498dd2691570219a841e330645c5c752dd
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-11-14 19:51:15 -0500

    Removed a bunch of JS packages we don’t use and switched datatables over to the “new” package name

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ var elixir = require('laravel-elixir'),
         'nestable-fork/dist/jquery.nestable.min.css',
         'admin.css',
         'mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css',
-        'datatables/media/css/dataTables.bootstrap.min.css',
-	'datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.css',
+        'datatables.net-bs/css/dataTables.bootstrap.min.css',
+	    'datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.css',
         'datatables.net-responsive-bs/css/responsive.bootstrap.css',
         'morris.js/morris.css'
     ],
@@ -20,18 +20,16 @@ var elixir = require('laravel-elixir'),
         'jquery/dist/jquery.min.js',
         'vue/dist/vue.min.js',
 	'js-cookie/src/js.cookie.js',
-        'nestable-fork/dist/jquery.nestable.min.js',
         'bootstrap/dist/js/bootstrap.min.js',
         'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
         'mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js',
-        'datatables/media/js/jquery.dataTables.min.js',
-        'datatables/media/js/dataTables.bootstrap.min.js',
+        'datatables.net/js/jquery.dataTables.min.js',
+        'datatables.net-bs/js/dataTables.bootstrap.min.js',
         'datatables-buttons/js/dataTables.buttons.js',
         'datatables-buttons/js/buttons.bootstrap.js',
         'datatables.net-fixedheader/js/dataTables.fixedHeader.js',
         'datatables.net-responsive/js/dataTables.responsive.js',
         'datatables.net-responsive-bs/js/responsive.bootstrap.js',
-        'morris.js/morris.js',
         'admin-lte/dist/js/app.min.js'
     ],
     applicationLess  = [
@@ -58,12 +56,8 @@ elixir(function(mix) {
         .sass('admin-buttons.scss', 'public/css/admin-buttons.css')
         .copy('resources/assets/datatables/buttons.server-side.js', 'public/js/buttons.server-side.js')
         .copy('resources/assets/js/admin.js', 'public/js/admin-custom.js')
-        .copy(bowerDir + 'raphael/raphael-min.js', 'public/js/raphael.js')
-        .copy(bowerDir + 'tinymce', 'public/packages/tinymce')
-        .copy(bowerDir + 'tinymce-localautosave/localautosave', 'public/packages/tinymce/plugins/localautosave')
         .copy(bowerDir + 'font-awesome/fonts', 'public/build/fonts')
         .copy(bowerDir + 'bootstrap/fonts', 'public/build/fonts')
-        .copy(bowerDir + 'mjolnic-bootstrap-colorpicker/dist/img/*', 'public/build/img')
         .less('application.less', bowerDir + 'application.css', { paths: applicationLess })
         .styles(applicationCss, 'public/css/application.css', bowerDir)
         .scripts(applicationJs, 'public/js/application.js', bowerDir)
