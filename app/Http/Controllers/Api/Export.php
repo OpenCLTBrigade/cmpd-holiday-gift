dce0f3259667282b0f237bc6f11bc2eccf95ff23
Etienne Laurin <etienne@atnnn.com>
2016-11-21 03:29:30 -0500

    prototype excel export

---
+++ app/Http/Controllers/Api/Export.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+use PHPExcel;
+use PHPExcel_Worksheet;
+use PHPExcel_Writer_Excel2007;
+
+class Export extends Controller {
+  public function export_data_excel(Request $request) {
+      header('Content-type: application/vnd.ms-excel');
+      header('Content-Disposition: attachment; filename="GiftProjectDump_' . date("YmdHis") . '.xslx"');
+      header('Cache-Control: max-age=0');
+
+      $excel = new PHPExcel();
+
+      $add = function(&$array, $table, $row, $key) {
+          if($key == 'password') return;
+          $array[] = $row->getAttributeValue($key);
+      };
+
+      foreach(['App\Household', 'App\HouseholdAddress', 'App\Child', 'App\HouseholdPhone', 'App\User'] as $model) {
+          $table = (new $model)->getTable();
+          $sheet = new PHPExcel_Worksheet($excel, $table);
+          $columns = \Schema::getColumnListing($table);
+          $sheet->fromArray($columns, NULL, 'A1');
+          $i = 2;
+          foreach($model::all() as $row) {
+              $array = array();
+              foreach($columns as $key) {
+                  $add($array, $table, $row, $key);
+              }
+              $sheet->fromArray($array, NULL, 'A' . $i++);
+          }
+          $cells = $sheet->getRowIterator()->current()->getCellIterator();
+          $cells->setIterateOnlyExistingCells(true);
+          foreach ($cells as $cell) {
+              $sheet->getColumnDimension($cell->getColumn())->setAutoSize(true);
+          }
+          $excel->addSheet($sheet);
+      }
+
+      $excel->removeSheetByIndex(0);
+
+      $writer = new PHPExcel_Writer_Excel2007($excel);
+      $writer->setOffice2003Compatibility(true);
+      $writer->save('php://output');
+
+      // TODO: How else can I tell Laravel to let me write the response?
+      flush();
+      exit(0);
+  }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -46,6 +46,7 @@ Route::group(['prefix' => 'api', 'namespace' => 'Api', 'middleware' => ['api', '
     Route::get('affiliation/schools', 'AffiliationController@schools');
     Route::get('cmpd_info', ['uses' => 'CmpdDivision@info']);
     Route::post('upload_household_form_file', ['uses' => 'HouseholdController@upload_attachment']);
+    Route::get('export_data_excel', ['uses' => 'Export@export_data_excel']);
 });
 
 // Admin routes
