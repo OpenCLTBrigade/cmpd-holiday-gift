86aaf2c26c30517e680baf1e4b3a6e4f971cd214
John Vincent <jbvincent@Johns-MacBook-Pro.local>
2016-12-13 21:09:40 -0500

    Commented out color picker and hide Submit button until draft

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -4,9 +4,9 @@ $(function () {
         todayHighlight: true
     });
 
-    $('#color').colorpicker().on('changeColor.colorpicker', function (event) {
+    /*$('#color').colorpicker().on('changeColor.colorpicker', function (event) {
         $(this).css('background-color', event.color.toHex());
-    });
+    });*/
 
     if (!$('input[type=date]').val()) {
         $('input[type=date]').datepicker("setDate", new Date());
