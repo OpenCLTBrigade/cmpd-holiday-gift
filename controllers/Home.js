8f5d323f0c1dbc0ed885f46479afb9a3c62ce0ef
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2017-04-18 22:17:05 -0400

    Testing encrypted fields

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: "Home",
   run: function(req, res, next) {
+		console.log(req.user.firstname);
 		res.render('home');
 	}
 }
--- models/user.js
+++ models/user.js
@@ -1,4 +1,9 @@
 module.exports = function(sequelize, Sequelize) {
+    var EncryptedField = require('sequelize-encrypted');
+    // secret key should be 32 bytes hex encoded (64 characters)
+    var key = "53E19CAB12F077ECDCC03C01BC621C8E950F9198C568A41A6DFDCE2E2D155469";
+    var enc_fields = EncryptedField(Sequelize, key);
+    
     var User = sequelize.define('user', {
         id: {
             autoIncrement: true,
@@ -35,7 +40,12 @@ module.exports = function(sequelize, Sequelize) {
         status: {
             type: Sequelize.ENUM('active', 'inactive'),
             defaultValue: 'active'
-        }
+        },
+        encrypted: enc_fields.vault('encrypted'),
+        eggo: enc_fields.field('eggo', {
+            type: Sequelize.TEXT,
+            defaultValue: null
+        }),
     });
     return User;
 }
