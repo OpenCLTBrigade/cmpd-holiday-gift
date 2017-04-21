96656331c548a4457083cbe12289f765bb14b830
Etienne Laurin <etienne@atnnn.com>
2017-04-21 21:44:09 +0000

    firstname -> name_first

--- controllers/Dashboard.js
+++ controllers/Dashboard.js
@@ -1,3 +1,3 @@
 module.exports = function (req, res) {
-    res.renderData('dashboard', 'Dashboard', {name: req.user.firstname});
+    res.renderData('dashboard', 'Dashboard', {name: req.user.name_first});
 };
