ad271c6798f3a5b640532127b01f0b2af786c13e
Etienne Laurin <etienne@atnnn.com>
2017-04-23 05:03:47 +0000

    add comments

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ var config = require('./config');
 var webpackConfig = require('./config/webpack');
 var models = require('./models');
 
-// An empty Express application
+// Start with an empty Express application
 var app = express();
 
-// A custom view rendering engine that uses Vue and Webpack
-
+// The skeleton is a raw HTML file with two elements that get replaced:
+// * {{{head}}} -- The title gets placed here
+// * {{{body}}} -- The view's template gets rendered here
 var skeleton = () =>
     fs.readFileSync(path.join(__dirname, 'views/skeleton.html')).toString();
 
+// In production mode, do not reload skeleton.html every request
 if (config.mode == 'production') {
     var skeletonContents = skeleton();
     skeleton = () => skeletonContents;
@@ -31,18 +33,28 @@ if (config.mode == 'production') {
 
 var viewDir = path.join(__dirname, 'views');
 
+// Each compiled view is assigned to a javascript variable by webpack
+// For example, `view/login.vue` is placed in `view_login`
 function viewPathToVar(path) {
     return path.replace(/^\/|\.vue$/g, '').replace(/[\/-]/g, '_');
 }
 
 app.set('views', viewDir);
 
+// res.render(view_name, options): a custom rendering engine using Vue and Webpack
+// Don't use this directly: use res.renderData instead
+// * options.title: the page title
+// * options.data: input data for the view, passed as JSON into the view's props
 app.engine('vue', function (filePath, options, callback) {
     if (filePath.slice(0, viewDir.length) != viewDir) {
         return callback('Could not load view from outside view dir: ' + filePath);
     }
+    // Don't open the `.vue` file, just use the filename to identify
+    // where webpack has placed it
     var path = filePath.slice(viewDir.length);
     var view = viewPathToVar(path);
+
+    // Fill in the blanks from `skeleton.html`
     var out = skeleton().replace(/\{\{\{([^}]*)\}\}\}/g, function (match, name) {
         if (name == 'head') {
             if (options.title) {
@@ -50,6 +62,7 @@ app.engine('vue', function (filePath, options, callback) {
             }
             return '';
         } else if (name == 'body') {
+            // Add a script that loads and renders the template
             return `
                 <div id="view"></div>
                 <script src="views/${view}.js"></script>
@@ -82,7 +95,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // res.renderData(view, title, data)
 //  - if the url ends with '.json', return data as JSON
 //  - if 'Accepts: application/json', return data as JSON
-//  - otherwise render the data using the view and title
+//  - otherwise render the view using `res.render`
 app.use(function (req, res, next) {
     if (req.accepts(['html', 'json']) == 'json') {
         req.wantsJSON = true;
@@ -104,7 +117,7 @@ app.use(function (req, res, next) {
     return next();
 });
 
-// Sessions
+// Enable persistent sessions
 var sequelizeSessionStore = new SessionStore({db: models.sequelize});
 app.use(cookieParser());
 app.use(expressSession({
@@ -114,27 +127,32 @@ app.use(expressSession({
     saveUninitialized: true
 }));
 
-// Authentication
+// Add authentication
 app.use(passport.initialize());
 app.use(passport.session());
 configurePassport(passport, models.user);
 
-// Routes
+// Load the routess
 app.use(require('./routes'));
 
-// Sync Database
+// Sync the database
 models.sequelize.sync().then(function () {
     console.log('Nice! Database looks fine');
 }).catch(function (err) {
     console.log(err, 'Something went wrong with the Database Update!');
 });
 
-// Generate assets
+// Generate the assets
 // TODO: use vuejs hot reload
+
+// List all available vues
 var allViews = {};
 glob.sync('views/**/*.vue').forEach(function (path) {
     allViews[viewPathToVar(path.replace('views/', ''))] = './' + path;
 });
+
+// Compile each view
+// In development mode, watch for changes and automatically recompile
 var compiler = webpack(Object.assign({}, webpackConfig, {entry: allViews}));
 var build = (cb) => (config.mode == 'development' ? compiler.watch({}, cb) : compiler.run(cb));
 build((err, stats) => {
@@ -154,4 +172,3 @@ build((err, stats) => {
 app.listen(config.port, function () {
     console.log('Express server listening on port ' + config.port);
 });
-
