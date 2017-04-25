4c119e6433ac307248247803a731a7ac82245411
“John <jbvincent006@gmail.com>
2017-04-25 19:19:45 -0400

    Adding data grid

--- views/household/list.vue
+++ views/household/list.vue
@@ -1,6 +1,10 @@
 <template>
   <main-layout :user="user" current_section="households" title="Households">
     <h2>All households</h2>
+    <table class="table table-striped table-hover table-bordered">
+      <thead>
+        <tr>
+          <th v-for="key in columns">
     <ul>
       <li v-for="household in households">
         <a :href="`/household/${household.id}/edit`">
