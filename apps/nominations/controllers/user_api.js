1aa88dc794597162128371e213989a3d3283fc30
Andrew Natoli <anatoli@xceleration.com>
2017-05-31 18:25:06 -0400

    Checkpoint for building API endpoint module

--- apps/nominations/controllers/user_api.js
+++ apps/nominations/controllers/user_api.js
@@ -2,9 +2,25 @@ var db = require('../../../models');
 
 module.exports = {
   list: async (req, res) => {
-    var users = await db.user.findAll();
-    res.json({
-      data: users
+    let perPage = 2;
+    let offset = (!req.query.page || isNaN(req.query.page)) ? 0 : req.query.page;
+    var users = await db.user.findAndCountAll({
+      limit: perPage,
+      offset: (offset - 1) * perPage
     });
+
+    let resultSet = {
+      total: users.count,
+      per_page: perPage,
+      current_page: offset,
+      last_page: 'x',
+      next_page_url: 'x',
+      prev_page_url: 'x',
+      'from': offset,
+      'to': 'x',
+      data: users.rows
+    }
+
+    res.json(resultSet);
   }
 };
