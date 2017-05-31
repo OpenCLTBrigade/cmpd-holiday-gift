e4a531cdf9912ae5af88852a4be28725622c5503
Andrew Natoli <anatoli@xceleration.com>
2017-05-30 20:05:13 -0400

    Checkpoint - Installed VueTable 2

--- apps/nominations/routes.js
+++ apps/nominations/routes.js
@@ -1,25 +1,23 @@
-var express = require('express');
+var Express = require('express');
+var { Household, User, Auth, Dashboard } = require('./controllers');
 
-var dashboard = require('./controllers/dashboard'),
-    auth = require('./controllers/auth'),
-    household = require('./controllers/household');
+let router = Express.Router();
+router.get('/', Auth.isLoggedIn, Dashboard);
 
-var router = express.Router();
-
-router.get('/', auth.isLoggedIn, dashboard);
-
-router.get('/login', auth.login.get);
-router.post('/login', auth.login.post);
-router.get('/logout', auth.logout);
-router.get('/register', auth.register.get);
-router.post('/register', auth.register.post);
-router.get('/register/confirm_email', auth.confirm.get);
+router.get('/login', Auth.login.get);
+router.post('/login', Auth.login.post);
+router.get('/logout', Auth.logout);
+router.get('/register', Auth.register.get);
+router.post('/register', Auth.register.post);
+router.get('/register/confirm_email', Auth.confirm.get);
 
 // TODO: require more than isLoggedIn for these tasks
-router.get('/household/new', auth.isLoggedIn, household.create.get);
-router.post('/household/new', auth.isLoggedIn, household.create.post);
-router.get('/household/:id/edit', auth.isLoggedIn, household.edit.get);
-router.post('/household/:id/edit', auth.isLoggedIn, household.edit.post);
-router.get('/households', auth.isLoggedIn, household.list);
+router.get('/household/new', Auth.isLoggedIn, Household.create.get);
+router.post('/household/new', Auth.isLoggedIn, Household.create.post);
+router.get('/household/:id/edit', Auth.isLoggedIn, Household.edit.get);
+router.post('/household/:id/edit', Auth.isLoggedIn, Household.edit.post);
+router.get('/households', Auth.isLoggedIn, Household.list);
+
+router.get('/api/user', Auth.isLoggedIn, User.list);
 
 module.exports = router;
