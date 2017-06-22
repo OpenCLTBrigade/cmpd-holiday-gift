edae46304b96f808238b7ef25caaf0ca5088082e
Etienne Laurin <etienne@atnnn.com>
2017-06-22 20:01:17 +0000

    package.json: delete unused deps and move frontend deps to dev

--- server/apps/nominations/controllers/user_api.js
+++ server/apps/nominations/controllers/user_api.js
@@ -1,8 +1,7 @@
-const VuetableApi = require('../../lib/vuetable-api');
-
 module.exports = {
   list: async (req, res) => {
-    let apiClient = new VuetableApi(req);
+    // TODO: table api
+    // let apiClient = new VuetableApi(req);
 
     var resultSet = await apiClient.fetch('user');
 
