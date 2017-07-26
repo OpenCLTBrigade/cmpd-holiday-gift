aaed6fee2871903d04e96200f75ea8e6fece9ff5
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2017-07-26 18:04:31 -0400

    Added household api

--- src/api/household.js
+++ src/api/household.js
@@ -1,6 +1,12 @@
 // @flow
-const householdApi = {
-  getHousehold: function (id: number) {
+import apiService from 'lib';
 
+const householdApi = {
+  getHousehold: function(householdId: number): Promise<any> {
+    return apiService.get(`/household/${householdId}`);
+  },
+
+  getHouseholdList: function(pageNumber: number = 0, search: ?string): Promise<any> {
+    return apiService.get('/household', { page: pageNumber, search: search });
   }
 };
