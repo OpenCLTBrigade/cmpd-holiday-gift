2b0fdf40e22c3948acfe05b6bc265441c0bc72f3
Jim Van Fleet <jim.van.fleet@levvel.io>
2017-10-04 13:36:26 -0400

    Adding database configurations

--- env.heroku.js
+++ env.heroku.js
@@ -25,13 +25,13 @@ module.exports = (config /*: * */) => { // eslint-disable-line
 
 // **** Sample MySQL Database ****
 
-//config.db = {
-//  dialect: 'mysql',
-//  host: 'localhost',
-//  user: 'username',
-//  password: '',
-//  database: 'gift'
-//};
+config.db = {
+ dialect: 'mysql',
+ host: process.env.DB_HOST,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_DATABASE
+};
 
 // **** Sample Production Mode Settings ****
 
