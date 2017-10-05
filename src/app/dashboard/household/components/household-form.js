0c9a41321e388f10167e3608b718089d5a502bed
Ryan <ryan.edge@zoho.com>
2017-10-05 01:05:53 -0400

    Add edit nomination functionality - If create, update when save draft is clicked w/ existing draft

--- src/app/dashboard/household/components/household-form.js
+++ src/app/dashboard/household/components/household-form.js
@@ -1,22 +1,32 @@
 // @flow
-import * as React from 'react';
+import * as React from 'react'
 
-import HouseholdForm from './form/head-of-household';
-import AddressForm from './form/address';
-import PhoneNumbers from './form/phone-numbers';
-import ChildForm from './form/child';
-import { Row, Col, Button } from 'react-bootstrap';
-import { Form } from 'neoform';
-import { FormValidation } from 'neoform-validation';
+import HouseholdForm from './form/head-of-household'
+import AddressForm from './form/address'
+import PhoneNumbers from './form/phone-numbers'
+import ChildForm from './form/child'
+import { Row, Col, Button } from 'react-bootstrap'
+import { Form } from 'neoform'
+import { FormValidation } from 'neoform-validation'
 
-const Household = ({ onSubmit, onInvalid, onSaveDraft, validate, data, addChild, removeChild, affiliations, saved }) => {
-  return (
+const Household = ({
+    onSubmit,
+    onInvalid,
+    onSaveDraft,
+    onUpdate,
+    validate,
+    data,
+    addChild,
+    removeChild,
+    affiliations,
+    saved
+}) => {
+    return (
         <form
             onSubmit={e => {
-              e.preventDefault();
-              validate(onSaveDraft, onInvalid);
-            }}
-        >
+                e.preventDefault()
+                validate(data.household && data.household.id ? onUpdate : onSaveDraft, onInvalid)
+            }}>
             <HouseholdForm />
             <AddressForm />
             <PhoneNumbers />
@@ -28,12 +38,12 @@ const Household = ({ onSubmit, onInvalid, onSaveDraft, validate, data, addChild,
             />
             <Row>
                 <Col xs={12}>
-                    <Button type="submit">Save Draft</Button>
+                    <Button type="submit">{data.household && data.household.id ? 'Update' : 'Save Draft'}</Button>
                     {saved && <Button onClick={onSubmit}>Submit Nomination</Button>}
                 </Col>
             </Row>
         </form>
-  );
-};
+    )
+}
 
-export default Form(FormValidation(Household));
+export default Form(FormValidation(Household))
--- src/app/dashboard/household/new-household.js
+++ src/app/dashboard/household/new-household.js
@@ -1,111 +1,123 @@
 // @flow
-import * as React from 'react';
+import * as React from 'react'
 
-import HouseholdForm from './components/household-form';
-import { setValue, getValue } from 'neoform-plain-object-helpers';
-import { getSchools } from 'api/affiliation';
-import { createHousehold, submitNomination, getHousehold } from 'api/household';
+import HouseholdForm from './components/household-form'
+import { setValue, getValue } from 'neoform-plain-object-helpers'
+import { getSchools } from 'api/affiliation'
+import { createHousehold, submitNomination, getHousehold, updateHousehold } from 'api/household'
 
-export default class NewHousehold
-    extends React.Component<
-        {},
-        {
-            household: {},
-            address: {},
-            nominations: Array<{}>,
-            schools: Array<mixed>,
-            phoneNumbers: Array<{}>,
-            saved: false
-        }
-    > {
-  constructor() {
-    super();
-
-    this.state = {
-      household: {},
-      address: {},
-      nominations: [],
-      schools: [],
-      phoneNumbers: [],
-      saved: false
-    };
-
-    (this: any).onChange = this.onChange.bind(this);
-    (this: any).onSubmit = this.onSubmit.bind(this);
-    (this: any).onSaveDraft = this.onSaveDraft.bind(this);
-  }
-
-  addChild() {
-    this.setState(() => {
-      return { nominations: this.state.nominations.concat({}) };
-    });
-  }
-
-  removeChild() {
-    const nominations = this.state.nominations.slice();
-    nominations.pop();
-    this.setState(() => {
-      return { nominations };
-    });
-  }
-
-  async componentDidMount() {
-    try {
-      const { id = undefined } = this.props.match && this.props.match.params;
-
-      if (id) {
-        const household = await getHousehold(id);
-        console.log(household);
-      }
-      const { items: schools } = await getSchools();
-
-      this.setState(() => ({ schools }));
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  onChange(name: string, value: any) {
-    this.setState(prevState => {
-      const newState = setValue(prevState, name, value);
-
-      return newState;
-    });
-  }
-
-  onInvalid() {
-    console.log('onInvalid');
-  }
-
-  reset() {
-    this.setState(() => {
-      return {
+export default class NewHousehold extends React.Component<
+    {},
+    {
         household: {},
         address: {},
-        nominations: [],
-        schools: [],
-        phoneNumbers: [],
+        nominations: Array<{}>,
+        schools: Array<mixed>,
+        phoneNumbers: Array<{}>,
         saved: false
-      };
-    });
-  }
+    }
+> {
+    constructor() {
+        super()
 
-  onSaveDraft() {
-    createHousehold(this.state).then(({ id }) => this.setState({ saved: true, id: id }));
-  }
+        this.state = {
+            household: {},
+            address: {},
+            nominations: [],
+            schools: [],
+            phoneNumbers: [],
+            saved: false
+        }
+        ;(this: any).onChange = this.onChange.bind(this)
+        ;(this: any).onSubmit = this.onSubmit.bind(this)
+        ;(this: any).onUpdate = this.onUpdate.bind(this)
+        ;(this: any).onSaveDraft = this.onSaveDraft.bind(this)
+    }
 
-  onSubmit() {
-    submitNomination({ id: this.state.id }).then(() => this.reset());
-  }
+    addChild() {
+        this.setState(() => {
+            return { nominations: this.state.nominations.concat({}) }
+        })
+    }
 
-  render(): React.Node {
-    return (
+    removeChild() {
+        const nominations = this.state.nominations.slice()
+        nominations.pop()
+        this.setState(() => {
+            return { nominations }
+        })
+    }
+
+    async componentDidMount() {
+        try {
+            const { id = undefined } = this.props.match && this.props.match.params
+            if (id) {
+                const household = await getHousehold(id)
+                const { children: nominations = [], phoneNumbers = [], address = {} } = household
+
+                this.setState(() => ({ household, nominations, phoneNumbers, address }))
+            }
+
+            const { items: schools } = await getSchools()
+
+            this.setState(() => ({ schools }))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    onChange(name: string, value: any) {
+        this.setState(prevState => {
+            const newState = setValue(prevState, name, value)
+
+            return newState
+        })
+    }
+
+    onInvalid() {
+        console.log('onInvalid')
+    }
+
+    reset() {
+        this.setState(() => {
+            return {
+                household: {},
+                address: {},
+                nominations: [],
+                schools: [],
+                phoneNumbers: [],
+                saved: false
+            }
+        })
+    }
+
+    onSaveDraft() {
+      const {id = undefined} = this.state
+      if(id) {
+        updateHousehold(id, this.state).then(() => console.log('updated hosehold')) 
+      } else {
+        createHousehold(this.state).then(({ id }) => this.setState({ saved: true, id: id }))
+      }
+    }
+
+    onUpdate() {
+      let {id} = this.state.household && this.state.household.id
+      updateHousehold(id, this.state).then(() => console.log('updated hosehold'))
+    }
+
+    onSubmit() {
+        submitNomination({ id: this.state.id }).then(() => this.reset())
+    }
+
+    render(): React.Node {
+        return (
             <div>
                 <HouseholdForm
                     data={this.state}
                     getValue={getValue}
                     onChange={this.onChange}
                     onSubmit={this.onSubmit}
+                    onUpdate={this.onUpdate}
                     onSaveDraft={this.onSaveDraft}
                     addChild={this.addChild.bind(this)}
                     removeChild={this.removeChild.bind(this)}
@@ -113,6 +125,6 @@ export default class NewHousehold
                     saved={this.state.saved}
                 />
             </div>
-    );
-  }
+        )
+    }
 }
