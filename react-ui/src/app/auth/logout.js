5a5dc6d6aee083069c1642f50970f47d05a723b7
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2017-10-20 00:56:36 -0400

    GIFT-216

--- react-ui/src/app/auth/logout.js
+++ react-ui/src/app/auth/logout.js
@@ -9,9 +9,11 @@ export default class Logout extends React.Component<{}> {
   constructor() {
     super();
     AuthToken.logout();
+    window.location.href = '/'; // Hard navigation to login screen. Dirty fix for GIFT-216
   }
 
   render(): React.Node {
-    return <Redirect to="/auth/login"/>;
+    // return <Redirect to="/auth/login"/>;
+    return null;
   }
 }
