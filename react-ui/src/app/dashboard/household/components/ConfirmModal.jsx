012ed0ae435b64141a0ea45ad0c83a4fbfe9edab
Ryan <ryan.c.edge@outlook.com>
2017-10-26 20:18:30 -0400

    Added confirmation modal to EditHousehold
    - rename ErrorModal

---
+++ react-ui/src/app/dashboard/household/components/ConfirmModal.jsx
@@ -0,0 +1,38 @@
+// @flow
+import * as React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+type Props = {
+    title: string,
+    message: string,
+    show: boolean,
+    handleClose: () => void
+}
+
+export default (props: Props) => {
+  const {
+        show,
+        title = 'Saved successfully',
+        message = 'Your nomination has been saved successfully.',
+        onReject,
+        onConfirm,
+        rejectText = 'Reject',
+        confirmText = 'Confirm',
+    } = props;
+  return (
+        <Modal show={show}>
+            <Modal.Header>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+                <Button onClick={onReject}>
+                    {rejectText}
+                </Button>
+                <Button onClick={onConfirm} bsStyle="primary">
+                    {confirmText}
+                </Button>
+            </Modal.Footer>
+        </Modal>
+  );
+};
