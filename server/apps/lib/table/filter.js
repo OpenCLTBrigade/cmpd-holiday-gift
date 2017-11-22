bd9e0ac6732acb0022fa58de079d5991acc9defe
Ryan <ryan.c.edge@outlook.com>
2017-11-21 23:28:43 -0500

    Add refactored table with filtering

---
+++ server/apps/lib/table/filter.js
@@ -0,0 +1,16 @@
+const Fuse = require('fuse.js');
+const defaults = {
+  shouldSort: true,
+  threshold: 0.2,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1
+};
+
+module.exports = ({ list, keys, search }) => {
+
+  const options = { ...defaults, keys };
+  const fuse = new Fuse(list, options);
+  return fuse.search(search);
+};
