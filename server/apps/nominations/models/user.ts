14ea4133b9a058483de9ee747426fd98286f3cde
Ryan <ryan.c.edge@outlook.com>
2017-12-21 01:06:30 -0500

    Updated user model.

--- server/apps/nominations/models/user.ts
+++ server/apps/nominations/models/user.ts
@@ -1,49 +1,52 @@
-import { Entity, BeforeInsert, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import Household from './household';
 
-@Entity()
-export class User {
+@Entity('users')
+export default class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({name: 'name_first'})
+  @Column('text', {name: 'name_first'})
   firstName: string
 
-  @Column({name: 'name_last'})
+  @Column('text', {name: 'name_last'})
   lastName: string
 
-  @Column({nullable: true})
+  @Column('text', {nullable: true})
   role: string
 
-  @Column({nullable: true})
+  @Column('text', {nullable: true})
   rank: string
 
-  @Column({nullable: true})
+  @Column('text', {nullable: true})
   phone: string
 
-  @Column()
+  @Column('text')
   email: string
 
-  @Column()
+  @Column('text')
   password: string
 
-  @Column()
+  @Column('text')
   active: boolean
 
-  @Column({name: 'nomination_limit', default: 5})
+  @Column('text', {name: 'nomination_limit', default: 5})
   nominationLimit: number
 
-  @Column({name: 'confirmation_email', default: false})
+  @Column('text', {name: 'confirmation_email', default: false})
   confirmationEmail: boolean
 
-  @Column({name: 'confirmation_code', nullable: true})
+  @Column('text', {name: 'confirmation_code', nullable: true})
   confirmationCode: string
 
-  @Column({name: 'email_verified', default: false})
+  @Column('text', {name: 'email_verified', default: false})
   emailVerified: boolean
 
-  @Column({default: false})
+  @Column('text', {default: false})
   approved: boolean
 
+  @OneToMany(() => Household, household => household.nominator)
+  households: Household[]
 
   @BeforeInsert() 
   encrypt() {
