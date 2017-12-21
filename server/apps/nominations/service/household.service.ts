0550f4ae03f4b2f36f9d89f66cdec1dcbde18631
Ryan <ryan.c.edge@outlook.com>
2017-12-21 09:48:52 -0500

    Add household service.

---
+++ server/apps/nominations/service/household.service.ts
@@ -0,0 +1,38 @@
+import { getRepository } from "typeorm";
+
+import Household from '../models/household';
+
+import logger from "../../lib/logger";
+import { createPagedResults } from "../../lib/table/table";
+
+
+export async function query({
+    page,
+    search,
+    active = true,
+    nominator = undefined,
+    baseUrl = '',
+    whitelist = []
+  }) {
+    try {
+      const query = search && {
+        keys: [
+            'name_last',
+            'nominator.name_last'
+          ],
+        search
+      };
+  
+      let results = await getRepository(Household).find();
+      return createPagedResults({
+        results,
+        page,
+        query,
+        baseUrl,
+        fieldWhitelist: whitelist
+      });
+    } catch (error) {
+      logger.error(error);
+      return undefined;
+    }
+  }
