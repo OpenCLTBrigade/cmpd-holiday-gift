14cdab1e5e9b03b7293193d2dedd762af12c3c95
Ryan <ryan.c.edge@outlook.com>
2017-12-21 09:48:24 -0500

    Changed initializer to typeorm

--- server/instances/sequelize.ts
+++ server/instances/sequelize.ts
@@ -1,25 +1,21 @@
-import * as Sequelize from 'sequelize'
-
+import { createConnection } from 'typeorm';
 import config from '../config';
+import Affiliation from '../apps/nominations/models/affiliation'
+import Attachment from '../apps/nominations/models/attachment'
+import Household from '../apps/nominations/models/household'
+import PhoneNumber from '../apps/nominations/models/phone-number'
+import Address from '../apps/nominations/models/address'
+import Child from '../apps/nominations/models/child'
+import User from '../apps/nominations/models/user'
+import AuthUser from '../apps/auth/user'
 
-function connect(dbConfig) {
-    return new Sequelize(
-        dbConfig.database,
-        dbConfig.user,
-        dbConfig.password,
-        Object.assign(
-          {
-            // TODO: move pool settings to config file
-            pool: {
-              max: 5,
-              min: 0,
-              idle: 10000
-            },
-            define: { underscored: true }
-          },
-          dbConfig
-        )
-      );
+async function connect({db: {dialect: type, storage: database, logging, ...rest}}) {
+  
+  return await createConnection({type, database, entities: [Affiliation, AuthUser, User, Attachment, Household, PhoneNumber, Address, Child]});
 }
 
-export default connect(config)
+
+
+export default async function() {
+  return await connect(config)
+}
