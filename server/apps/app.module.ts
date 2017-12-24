5e484a2906241cc2a17e8805766af596ef152972
Ryan <ryan.c.edge@outlook.com>
2017-12-24 00:08:27 -0500

    Added missing app module

---
+++ server/apps/app.module.ts
@@ -0,0 +1,28 @@
+import { Module, MiddlewaresConsumer, RequestMethod } from '@nestjs/common';
+import { NominationsModule } from './nominations/nominations.module';
+import { AuthModule } from './auth/auth.module';
+import auth from './lib/auth';
+import config from '../config'
+
+const allRoutes = {
+  path: '*', method: RequestMethod.ALL
+}
+
+const nominationRoutes = {
+  path: 'api/nominations/*', method: RequestMethod.ALL
+}
+
+const authRoutes = {
+  path: 'api/auth/*', method: RequestMethod.ALL
+}
+
+@Module({
+  modules: [AuthModule, NominationsModule]
+})
+export class AppModule {
+  configure(consumer: MiddlewaresConsumer): void {
+    consumer.apply(auth.authMiddleware(config.jwtSecrets.auth)).forRoutes(authRoutes);
+    consumer.apply(auth.authMiddleware(config.jwtSecrets.nominations)).forRoutes(nominationRoutes);
+    consumer.apply(auth.sessionMiddleware).forRoutes(allRoutes);
+  }
+}
