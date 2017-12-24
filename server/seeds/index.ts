a0c02b5c069ac69824cb123a66a29fb3bb3eeed7
Ryan <ryan.c.edge@outlook.com>
2017-12-23 20:33:19 -0500

    Added encryption subscriber to seed's db config

--- server/seeds/index.ts
+++ server/seeds/index.ts
@@ -3,27 +3,52 @@
 const fs = require('fs');
 const path = require('path');
 import config from '../config';
-import { Affiliation, Address, Attachment, Child, PhoneNumber, Household, User } from '../entities'
+import {
+  Affiliation,
+  Address,
+  Attachment,
+  Child,
+  PhoneNumber,
+  Household,
+  User
+} from '../entities';
 
-import { createConnection } from 'typeorm'
+import { createConnection } from 'typeorm';
+import { AutoEncryptSubscriber } from 'typeorm-encrypted';
 
-async function seed({db: {dialect: type, storage: database}}) {
-  const connection = await createConnection({type, database, entities: [Affiliation, Address, Attachment, Child, PhoneNumber, Household, User]})
+async function seed({ db: { dialect: type, storage: database } }) {
+  const connection = await createConnection({
+    type,
+    database,
+    synchronize: true,
+    entities: [
+      Affiliation,
+      Address,
+      Attachment,
+      Child,
+      PhoneNumber,
+      Household,
+      User
+    ],
+    subscribers: [AutoEncryptSubscriber]
+  });
 
   let seq = Promise.resolve();
   fs
-        .readdirSync(__dirname)
-        .filter(function (file) {
-          return file.match(/.ts$/) && (file !== 'index.ts');
-        })
-        .forEach(async function (file) {
-          if (config.verboseSeed) {
-            seq = seq.then(() => console.log(`* Seeding from ${file}`));
-          }
-          seq = seq.then(() => {
-            return import(path.join(__dirname, file)).then(({default: seed}) => seed(connection, config.verboseSeed))
-          });
-        });
+    .readdirSync(__dirname)
+    .filter(function(file) {
+      return file.match(/.ts$/) && file !== 'index.ts';
+    })
+    .forEach(async function(file) {
+      if (config.verboseSeed) {
+        seq = seq.then(() => console.log(`* Seeding from ${file}`));
+      }
+      seq = seq.then(() => {
+        return import(path.join(__dirname, file)).then(({ default: seed }) =>
+          seed(connection, config.verboseSeed)
+        );
+      });
+    });
   await seq;
 }
 
