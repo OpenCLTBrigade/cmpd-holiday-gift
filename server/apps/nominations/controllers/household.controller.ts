5028511f0333c2cea4fa3c37911e889d92587eba
Ryan <ryan.c.edge@outlook.com>
2017-12-24 00:07:43 -0500

    Add root household endpoint

--- server/apps/nominations/controllers/household.controller.ts
+++ server/apps/nominations/controllers/household.controller.ts
@@ -1,6 +1,21 @@
-import { Get, Controller, Post, Put, Delete } from '@nestjs/common';
+import { Get, Controller, Post, Put, Delete, Query, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '../../../common/guards/auth.guard';
 
-@Controller('nominations/household')
+import { query as queryHouseholds } from '../service/household.service';
+import { baseUrl } from '../../lib/misc'
+
+@Controller('api/nominations/households')
 export class HouseholdController {
-    
+    @Get()
+    @UseGuards(AuthGuard)
+    async getAll(@Query('search') search, @Query('page') page, @Req() req) {
+        console.log(page);
+        const results = await queryHouseholds({
+            page, 
+            search,
+            baseUrl: baseUrl(req)
+          })
+
+        return results
+    }
 }
