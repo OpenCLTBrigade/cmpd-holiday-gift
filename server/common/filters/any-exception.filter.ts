21233ac48c10d88b6d82a8873d856a6bcbda2bb6
Ryan <ryan.c.edge@outlook.com>
2018-01-03 01:28:32 -0500

    Only check for unauth error

--- server/common/filters/any-exception.filter.ts
+++ server/common/filters/any-exception.filter.ts
@@ -7,12 +7,5 @@ export class AnyExceptionFilter implements ExceptionFilter {
     if (exception.name === 'UnauthorizedError') {
         return response.status(401).send('invalid token...');
     }
-
-    response
-      .status(500)
-      .json({
-        statusCode: 500,
-        message: `It's a message from the exception filter`,
-      });
   }
 }
