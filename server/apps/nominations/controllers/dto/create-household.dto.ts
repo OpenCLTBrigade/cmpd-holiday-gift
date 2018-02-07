06e8505cf95d40162b889b0cb82d4e401757362c
Ryan Edge <ryan.c.edge@outlook.com>
2018-02-06 21:37:37 -0500

    Add swagger properties to household dtos

--- server/apps/nominations/controllers/dto/create-household.dto.ts
+++ server/apps/nominations/controllers/dto/create-household.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsNotEmpty, IsOptional, ArrayNotEmpty, ValidateNested } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 class AddressDto {
     readonly id
@@ -9,50 +10,64 @@ class AddressDto {
 
     readonly cmpdResponseArea
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly type: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly street: string
 
     readonly street2
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly city: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly state: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly zip: string
 }
 
 //TODO: Validate nomination limit
 export class CreateHouseholdDto {
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly firstName: string
 
+    @ApiModelProperty({type: String, required: false})
     @IsOptional()
     readonly middleName: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly lastName: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly dob: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly race: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly gender: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly last4ssn: string
 
+    @ApiModelProperty({type: String})
     @IsEmail()
     readonly email: string
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly preferredContactMethod: string
 
--- server/apps/nominations/controllers/dto/create-user.dto.ts
+++ server/apps/nominations/controllers/dto/create-user.dto.ts
@@ -1,40 +1,52 @@
 import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsBoolean, ArrayNotEmpty, ValidateNested } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly firstName: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly lastName: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly role: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly rank: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     @IsEmail()
     readonly email: string
     
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly phone: string
     
+    @ApiModelProperty({type: Boolean})
     @IsBoolean()
     readonly active: boolean
 
+    @ApiModelProperty({type: Number})
     @IsNotEmpty()
     @IsNumber()
     readonly nominationLimit: number
 
+    @ApiModelProperty({type: Number})
     @IsNotEmpty()
     @IsNumber()
     readonly affiliationId: number
 
+    @ApiModelProperty({type: Boolean})
     @IsOptional()
     readonly emailVerified: boolean
 
+    @ApiModelProperty({type: String})
     @IsNotEmpty()
     readonly password: string
 
