8da63d76ab97fc4234ba1829e73bfead64d77599
Ryan Edge <ryan.c.edge@outlook.com>
2018-02-07 16:44:23 -0500

    improved logging for routes.

--- server/common/interceptors/logging.interceptor.ts
+++ server/common/interceptors/logging.interceptor.ts
@@ -7,13 +7,17 @@ import logger from '../util/logger';
 @Interceptor()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(
-    dataOrRequest,
+    { originalUrl = '', method = '' },
     context: ExecutionContext,
     stream$: Observable<any>,
   ): Observable<any> {
-    logger.info('Before...');
+    logger.info('Before...', {method, originalUrl});
+
     const now = Date.now();
 
-    return stream$.do(() => logger.info(`After... ${Date.now() - now}ms`));
+    return stream$.do(
+      () => logger.info(`After... ${Date.now() - now}ms`, {method, originalUrl}),
+      ({message = ''}) => logger.error(`Error... ${Date.now() - now}ms`, {method, originalUrl, message})
+    );
   }
 }
