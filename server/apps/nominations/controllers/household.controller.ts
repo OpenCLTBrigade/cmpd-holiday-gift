a419c8c2eece5a003cb98e91dd6a0acfa2cc9dab
Ryan Edge <ryan.c.edge@outlook.com>
2018-02-07 16:45:41 -0500

    Add createAttachment route handler

--- server/apps/nominations/controllers/household.controller.ts
+++ server/apps/nominations/controllers/household.controller.ts
@@ -12,15 +12,24 @@ import {
     Res,
     UseGuards,
     ValidationPipe,
+    NotFoundException,
 } from '@nestjs/common';
+import * as fs from 'fs-extra';
+import { createAttachment, createMainBucket, getAttachmentUrl, getAttachments } from '../../lib/attachment';
+
+import { Request, Response } from 'express';
 import { AuthGuard } from '../../../common/guards/auth.guard';
 import { RolesGuard } from '../../../common/guards/roles.guard';
-
 import { query as queryHouseholds, getById, submitNomination, submitFeedback, updateHousehold, createHousehold, removeHousehold } from '../service/household.service';
 import { baseUrl } from '../../lib/misc'
 import { CreateHouseholdDto } from './dto/create-household.dto';
 import { UpdateHouseholdDto } from './dto/update-household.dto';
 import { Roles } from '../../../common/decorators/roles.decorator';
+import logger from '../../lib/logger';
+
+//TODO: better place to put this???
+const bucketName = process.env.S3_BUCKET_NAME || 'cfc-cmpd-explorers-qa';
+
 
 @UseGuards(RolesGuard)
 @Controller('api/nominations/households')
@@ -40,7 +49,11 @@ export class HouseholdController {
     @Get('/:id')
     @UseGuards(AuthGuard)
     async getById(@Param('id') id) {
-        return await getById(id);
+        const household = await getById(id);
+
+        if(!household) throw new NotFoundException();
+
+        return household;
     }
 
     @Post()
@@ -59,6 +72,34 @@ export class HouseholdController {
     @UseGuards(AuthGuard)
     async removeHousehold(@Param('id') id) {
         return await removeHousehold(id);
+    }
+
+    @Put(':id/upload')
+    async createAttachment(@Req() req: Request, @Res() res: Response, @Param('id') id: number) {
+        const fileResults = [];
+        const files = req.files; 
+        const owner = `household-${id}`;
+        
+        if(files instanceof Array) {
+            for (const file of files) {
+
+                const { filename, path } = file;
+                const fileBuffer = await fs.readFile(file.path);
+                await createAttachment({ name: bucketName, filename: `${owner}/${filename}`, fileBuffer });
+
+                logger.info('uploaded to s3', { filename });
+
+                //TODO: Add attachment to household
+                //addAttachment(id, { filename })
+
+                await fs.remove(file.path);
+
+                const url = await getAttachmentUrl({ name: bucketName, filename: `${owner}/${filename}` });
+                fileResults.push({ url, filename });
+            }
+        }
+        
+        return fileResults;
     }
 
     @Post('/submit')
