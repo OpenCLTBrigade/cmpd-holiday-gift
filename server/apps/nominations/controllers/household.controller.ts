b72f9e23117be3e48c1d8b876719623a4c26d89a
Ryan <ryan.edge@zoho.com>
2018-02-12 00:18:42 -0500

    Add attachment & feedback endpoints
    Update attachment entity

--- server/apps/nominations/controllers/household.controller.ts
+++ server/apps/nominations/controllers/household.controller.ts
@@ -13,19 +13,28 @@ import {
     UseGuards,
     ValidationPipe,
     NotFoundException,
+    BadRequestException,
+    InternalServerErrorException
 } from '@nestjs/common';
 import * as fs from 'fs-extra';
+import * as path from 'path';
+
 import { createAttachment, createMainBucket, getAttachmentUrl, getAttachments } from '../../lib/attachment';
 
 import { Request, Response } from 'express';
 import { AuthGuard } from '../../../common/guards/auth.guard';
 import { RolesGuard } from '../../../common/guards/roles.guard';
-import { query as queryHouseholds, getById, submitNomination, submitFeedback, updateHousehold, createHousehold, removeHousehold } from '../service/household.service';
+import { query as queryHouseholds, getById, submitNomination, submitFeedback, updateHousehold, createHousehold, removeHousehold, addAttachment } from '../service/household.service';
 import { baseUrl } from '../../lib/misc'
 import { CreateHouseholdDto } from './dto/create-household.dto';
 import { UpdateHouseholdDto } from './dto/update-household.dto';
 import { Roles } from '../../../common/decorators/roles.decorator';
 import logger from '../../lib/logger';
+import { SubmitNominationDto } from './dto/submit-nomination.dto';
+
+type AuthedRequest = {
+    user: {id}
+} & Request
 
 //TODO: better place to put this???
 const bucketName = process.env.S3_BUCKET_NAME || 'cfc-cmpd-explorers-qa';
@@ -34,6 +43,8 @@ const bucketName = process.env.S3_BUCKET_NAME || 'cfc-cmpd-explorers-qa';
 @UseGuards(RolesGuard)
 @Controller('api/nominations/households')
 export class HouseholdController {
+
+    //TODO: Explore using graphql here
     @Get()
     @UseGuards(AuthGuard)
     async getAll(@Query('search') search, @Query('page') page, @Req() req) {
@@ -75,36 +86,54 @@ export class HouseholdController {
     }
 
     @Put(':id/upload')
-    async createAttachment(@Req() req: Request, @Res() res: Response, @Param('id') id: number) {
+    @UseGuards(AuthGuard)
+    async createAttachments(@Req() {user: {id: userId}, files}: AuthedRequest, @Res() res: Response, @Param('id') id: number) {
         const fileResults = [];
-        const files = req.files; 
         const owner = `household-${id}`;
-        
-        if(files instanceof Array) {
-            for (const file of files) {
+        const uploadDir = path.join(process.cwd(), 'uploads');
 
-                const { filename, path } = file;
-                const fileBuffer = await fs.readFile(file.path);
-                await createAttachment({ name: bucketName, filename: `${owner}/${filename}`, fileBuffer });
-
-                logger.info('uploaded to s3', { filename });
-
-                //TODO: Add attachment to household
-                //addAttachment(id, { filename })
-
-                await fs.remove(file.path);
-
-                const url = await getAttachmentUrl({ name: bucketName, filename: `${owner}/${filename}` });
-                fileResults.push({ url, filename });
-            }
+        if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir);
         }
+
+        logger.info('uploading document for user', userId);
+
+        try {
+            await createMainBucket(bucketName);
+          } catch (error) {
+            logger.error(error);
+            throw new InternalServerErrorException();
+          }
+
+        try {     
+            if(files instanceof Array) {
+                for (const file of files) {
+
+                    const { filename, path } = file;
+                    const fileBuffer = await fs.readFile(path);
+                    await createAttachment({ name: bucketName, filename: `${owner}/${filename}`, fileBuffer });
+
+                    logger.info('uploaded to s3', { filename });
+
+                    await addAttachment({householdId: id, path: filename})
+
+                    await fs.remove(path);
+
+                    const url = await getAttachmentUrl({ name: bucketName, filename: `${owner}/${filename}` });
+                    fileResults.push({ url, filename });
+                }
+            }
         
-        return fileResults;
+            return fileResults;
+        } catch (error) {
+            logger.error(error)
+            throw new InternalServerErrorException();
+        }
     }
 
     @Post('/submit')
     @UseGuards(AuthGuard)
-    async submitNomination(@Param('id') id, @Res() res) {
+    async submitNomination(@Param('id') id, @Res() res: Response) {
         await submitNomination(id);
 
         res.status(HttpStatus.OK).send();
@@ -113,8 +142,8 @@ export class HouseholdController {
     @Roles('admin')
     @Post(':id/feedback')
     @UseGuards(AuthGuard)
-    async submitFeedback(@Param('id') id, @Res() res) {
-        await submitFeedback(id);
+    async submitFeedback(@Param('id') id, @Res() res, @Body(new ValidationPipe()) submitFeedbackDto: SubmitNominationDto) {
+        await submitFeedback({id, ...submitFeedbackDto});
 
         res.status(HttpStatus.OK).send();
     }
