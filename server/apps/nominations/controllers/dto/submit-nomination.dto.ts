68bcbe64718868b575c850c4717119c2f6c6b0b9
Ryan <ryan.edge@zoho.com>
2018-02-11 23:50:13 -0500

    Add nomination dto

---
+++ server/apps/nominations/controllers/dto/submit-nomination.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
+
+export class SubmitNominationDto {
+    @ApiModelProperty({type: String})
+    @IsOptional()
+    readonly reason: string = ''
+
+    @ApiModelProperty({type: String})
+    @IsNotEmpty()
+    readonly message: string
+   
+    @ApiModelProperty({type: Boolean})
+    @IsNotEmpty()
+    readonly approved: boolean
+}
