1ef490ffdadea40e5636f4f384af17b41fa030d4
Ryan <ryan.c.edge@outlook.com>
2018-02-14 02:34:26 -0500

    Add tests for affilation controller
    - Make service injectable
    - Add application error

---
+++ server/common/util/application-error.ts
@@ -0,0 +1,17 @@
+export class ApplicationError extends Error {
+    constructor(message?: string, public code?: string) {
+        super(message); // 'Error' breaks prototype chain here
+        Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+    }
+}
+
+export const handleErrors = (errorMap) =>(error: Error) => {
+    if(error instanceof ApplicationError) {
+        const { code = "default" } = error;
+        const ErrorToThrow = errorMap[code];
+
+        throw new ErrorToThrow();
+    }
+
+    throw error;
+}
