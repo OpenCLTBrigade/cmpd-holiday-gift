162efc5a492e69d96a352436a4dcf38553802aec
Ryan <ryan.c.edge@outlook.com>
2018-02-15 21:46:58 -0500

    Add affiliation to nominator.

--- server/entities/nominator.ts
+++ server/entities/nominator.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, OneToMany } from 'typeorm';
+import Affiliation from './affiliation';
+import { Entity, Column, OneToMany, OneToOne, JoinColumn } from 'typeorm';
 import Household from './household';
 import AbstractUser from './abstract/user'
 
@@ -9,6 +10,10 @@ export default class Nominator extends AbstractUser {
 
     Object.assign(this, props);
   }
+
+  @OneToOne(() => Affiliation)
+  @JoinColumn({ name: 'affiliation_id' })
+  affiliation: Affiliation 
 
   @Column('int', {name: 'nomination_limit', default: 5})
   nominationLimit: number
