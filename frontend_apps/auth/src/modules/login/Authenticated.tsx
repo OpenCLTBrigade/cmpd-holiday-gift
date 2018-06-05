aa5c2f40f335e317320f0083005510c6a56b3890
Ryan <ryan.c.edge@outlook.com>
2018-06-05 00:18:21 -0400

    Only redirect if referrer is different

--- frontend_apps/auth/src/modules/login/Authenticated.tsx
+++ frontend_apps/auth/src/modules/login/Authenticated.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Redirect } from 'react-router';
 
-export const Authenticated = ({ location }) => (
-  <Redirect
-    to={{
-      pathname: location.referrer
-    }}
-  />
-);
+export const Authenticated = ({ location }) =>
+  location.referrer !== location.url ? (
+    <Redirect
+      to={{
+        pathname: location.referrer
+      }}
+    />
+  ) : (
+    <div>You have been successfully logged in</div>
+  );
