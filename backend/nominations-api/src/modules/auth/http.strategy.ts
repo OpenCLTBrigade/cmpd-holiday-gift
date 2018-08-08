41634d7cb432fddc08a880ae9298ff8b708a4ef6
Ryan <ryan.c.edge@outlook.com>
2018-08-07 20:38:32 -0400

    Add some logging to http strategy

--- backend/nominations-api/src/modules/auth/http.strategy.ts
+++ backend/nominations-api/src/modules/auth/http.strategy.ts
@@ -2,6 +2,7 @@ import { Strategy } from 'passport-http-bearer';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AccountService } from './v2/account.service';
+import { logger } from 'cmpd-common-api';
 
 @Injectable()
 export class HttpStrategy extends PassportStrategy(Strategy) {
@@ -10,17 +11,23 @@ export class HttpStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(token: any, done: Function) {
+    logger.info('validate token');
+
     try {
       const user = await this.accountService.validateUser(token);
 
       if (!user) {
+        logger.info('user not found');
+
         return done(new UnauthorizedException(), false);
       }
       if (user.disabled) {
+        logger.info('user found, but disabled');
         return done(new UnauthorizedException(), false);
       }
       done(null, user);
     } catch (error) {
+      logger.error('unknown auth error', error);
       return done(new UnauthorizedException(), false);
     }
   }
