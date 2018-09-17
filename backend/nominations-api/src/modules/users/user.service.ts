a84910ac194b09a7e43b45d3a04a5a0d4a689c90
Ryan <ryan.c.edge@outlook.com>
2018-09-16 22:31:13 -0400

    Return correct size on user queries & add search expression

--- backend/nominations-api/src/modules/users/user.service.ts
+++ backend/nominations-api/src/modules/users/user.service.ts
@@ -4,6 +4,7 @@ import { isNil } from 'rambda';
 import logger from '../lib/logger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Brackets } from 'typeorm';
 
 // TODO: Criteria that determines whether or not a user account is pending approval
 const Criteria = {
@@ -42,14 +43,19 @@ export class UserService {
         sqlQuery = sqlQuery.where('user.disabled = :disabled', { disabled: Criteria.LIVE.disabled });
       }
 
+      if (search) {
+        sqlQuery = sqlQuery.andWhere(searchExpression(search));
+      }
+
       const results = await sqlQuery.getMany();
+      const totalSize = await sqlQuery.getCount();
 
       //TODO: Revisit if we need paging
       return {
         items: results,
         page,
         baseUrl,
-        totalSize: results.length,
+        totalSize,
         per_page: results.length
       };
     } catch (error) {
@@ -60,11 +66,6 @@ export class UserService {
 
   async getPendingUsers({ page, search, baseUrl = '', whitelist = [] }) {
     try {
-      const searchQuery = search && {
-        keys: ['lastName', 'firstName'],
-        search
-      };
-
       let sqlQuery = Nominator.createQueryBuilder('user').leftJoinAndSelect('user.affiliation', 'affiliation');
 
       sqlQuery = sqlQuery
@@ -73,17 +74,26 @@ export class UserService {
         })
         .orWhere('user.email_verified = :email_verified', {
           email_verified: Criteria.PENDING.emailVerified
-        })
-        .skip((page - 1) * 10)
-        .take(10);
+        });
 
-      const results = await sqlQuery.getMany();
+      if (search) {
+        sqlQuery = sqlQuery.andWhere(searchExpression(search));
+      }
+
+      const totalSize = await sqlQuery.getCount();
+
+      const results = await sqlQuery
+        .skip((page - 1) * 10)
+        .take(10)
+        .printSql()
+
+        .getMany();
 
       return {
         items: results,
         page,
         baseUrl,
-        totalSize: results.length,
+        totalSize,
         per_page: results.length
       };
     } catch (error) {
@@ -114,3 +124,8 @@ export class UserService {
     logger.warn('this function was deprecated and moved to the auth module');
   }
 }
+
+const searchExpression = (search: string) =>
+  new Brackets(qb => {
+    qb.where('user.name like :name', { name: '%' + search + '%' });
+  });
