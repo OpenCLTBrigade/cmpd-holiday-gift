a6daa370ec92bbec5217e4f7d03951a66ac6b9fd
Ryan <ryan.c.edge@outlook.com>
2018-10-02 19:35:05 -0400

    Add logout button & prompt to login again

--- frontend/applications/auth/src/modules/register/NotApproved.tsx
+++ frontend/applications/auth/src/modules/register/NotApproved.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { css } from 'react-emotion';
+import { logout } from '../common/services/login';
 
 export const NotApproved = () => (
   <div
@@ -9,8 +10,14 @@ export const NotApproved = () => (
     <h1>Come back later!</h1>
     <p>Thanks for registering!</p>
     <p>
+      If you have already verified your email address, please contact your program manager so that they can approve you.
+    </p>
+    <p>
       You have completed registration but you're not able to log in just yet. A program manager will have to review and
       approve your account. You will receive an email once your account has been approved.
     </p>
+    <a href="#" onClick={logout}>
+      Back to login
+    </a>
   </div>
 );
