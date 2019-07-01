1436cfddf45c29376ca11b0257661754f700d8f0
Ryan Edge <chimon2000@users.noreply.github.com>
2019-07-01 15:33:25 -0400

    Added routes for unauthenticated app

--- frontend/applications/nominations/src/app/UnauthenticatedApp.tsx
+++ frontend/applications/nominations/src/app/UnauthenticatedApp.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './pages/Login/Login';
 
 export function UnauthenticatedApp() {
-  return <div>Not authenticated</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+        <Route path="/login" component={Login} />
+        <Route path="*" render={() => <Redirect to="/login" />} />
+      </Switch>
+    </Router>
+  );
 }
