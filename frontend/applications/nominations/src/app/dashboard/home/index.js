5642a1a7e470ef6b9a300ef6275fe8ee3881008e
Ryan Edge <chimon2000@users.noreply.github.com>
2019-07-02 19:37:54 -0400

    Fix welcome screen

--- frontend/applications/nominations/src/app/dashboard/home/index.js
+++ frontend/applications/nominations/src/app/dashboard/home/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import Box from '../components/box';
 import { redirectPostWithAuth } from '../../../lib/auth';
+import { useAuth } from '../../../contexts/Auth';
 
 function getReport(name) {
   return _event => {
@@ -12,94 +13,40 @@ function getReport(name) {
   };
 }
 
-export default class Home extends React.Component {
-  render() {
-    const { user } = this.props;
+export default function Home() {
+  const { user, claims } = useAuth();
+  const isAdmin = claims && claims['admin'] && claims['admin']['admin'];
 
-    if (!user || user.role !== 'admin') {
-      return (
-        <div>
-          <Row>
-            <Col xs={12}>
-              <Box title="Welcome!">
-                <p>Welcome to the CMPD Explorers Christmas Project Dashboard!</p>
-                <p>
-                  You can add and manage your nominations using the navigation menu to the left. If you do not see the
-                  menu, please click the menu button (three lines) at the top of the page.
-                </p>
-              </Box>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <Box title="Tips">
-                <ul>
-                  <li>When submitting a nomination you must first press save before you can upload the signed form.</li>
-                  <li>
-                    File uploads will occur automatically when you select a file. After a few seconds the "Scanned
-                    Forms" box will update with your uploaded file as a blue link.
-                  </li>
-                  <li>
-                    When you submit your nomination it may take a moment; please avoid double clicking on the submit /
-                    save buttons.
-                  </li>
-                  <li>
-                    If the form does not seem to be submitting, please scroll up and verify there are no fields outlined
-                    in red.
-                  </li>
-                </ul>
-              </Box>
-            </Col>
-          </Row>
-        </div>
-      );
-    }
-
+  if (!user || !isAdmin) {
     return (
       <div>
-        {/* Quick Overview */}
         <Row>
-          <Col md={6}>
-            <Box title="Quick Overview">Reporting tools and stats are under construction...</Box>
-          </Col>
-        </Row>
-        {/* Reports */}
-        <Row>
-          <Col md={6}>
-            <Box title="Reports">
-              <ul>
-                {/* TODO: only show restricted reports if user is admin */}
-                <li>
-                  <button onClick={getReport('all')}>Export data in Excel format</button>
-                </li>
-                <li>
-                  <button onClick={getReport('link')}>Generate Link Report</button>
-                </li>
-                <li>
-                  <button onClick={getReport('bikes')}>Generate Bicycle Report</button>
-                </li>
-                <li>
-                  <button onClick={getReport('division')}>Generate Division Report</button>
-                </li>
-              </ul>
+          <Col xs={12}>
+            <Box title="Welcome!">
+              <p>Welcome to the CMPD Explorers Christmas Project Dashboard!</p>
+              <p>
+                You can add and manage your nominations using the navigation menu to the left. If you do not see the
+                menu, please click the menu button (three lines) at the top of the page.
+              </p>
             </Box>
           </Col>
         </Row>
-        {/* Packing slips */}
         <Row>
-          <Col md={6}>
-            <Box title="Packing Slips">
-              Packing Slips
+          <Col xs={12}>
+            <Box title="Tips">
               <ul>
+                <li>When submitting a nomination you must first press save before you can upload the signed form.</li>
                 <li>
-                  <Link to="/slips/packing" target="_blank">
-                    All packing slips
-                  </Link>
+                  File uploads will occur automatically when you select a file. After a few seconds the "Scanned Forms"
+                  box will update with your uploaded file as a blue link.
                 </li>
                 <li>
-                  <Link to="/slips/bicycle" target="_blank">
-                    All bicycle slips
-                  </Link>
+                  When you submit your nomination it may take a moment; please avoid double clicking on the submit /
+                  save buttons.
+                </li>
+                <li>
+                  If the form does not seem to be submitting, please scroll up and verify there are no fields outlined
+                  in red.
                 </li>
               </ul>
             </Box>
@@ -108,4 +55,57 @@ export default class Home extends React.Component {
       </div>
     );
   }
+
+  return (
+    <div>
+      {/* Quick Overview */}
+      <Row>
+        <Col md={6}>
+          <Box title="Quick Overview">Reporting tools and stats are under construction...</Box>
+        </Col>
+      </Row>
+      {/* Reports */}
+      <Row>
+        <Col md={6}>
+          <Box title="Reports">
+            <ul>
+              {/* TODO: only show restricted reports if user is admin */}
+              <li>
+                <button onClick={getReport('all')}>Export data in Excel format</button>
+              </li>
+              <li>
+                <button onClick={getReport('link')}>Generate Link Report</button>
+              </li>
+              <li>
+                <button onClick={getReport('bikes')}>Generate Bicycle Report</button>
+              </li>
+              <li>
+                <button onClick={getReport('division')}>Generate Division Report</button>
+              </li>
+            </ul>
+          </Box>
+        </Col>
+      </Row>
+      {/* Packing slips */}
+      <Row>
+        <Col md={6}>
+          <Box title="Packing Slips">
+            Packing Slips
+            <ul>
+              <li>
+                <Link to="/slips/packing" target="_blank">
+                  All packing slips
+                </Link>
+              </li>
+              <li>
+                <Link to="/slips/bicycle" target="_blank">
+                  All bicycle slips
+                </Link>
+              </li>
+            </ul>
+          </Box>
+        </Col>
+      </Row>
+    </div>
+  );
 }
