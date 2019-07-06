4e86fc2acbd98130f9eac7b6aef6a57bab502304
Ryan Edge <chimon2000@users.noreply.github.com>
2019-07-06 15:40:05 -0400

    Added ability to upload attachments

---
+++ frontend/applications/nominations/src/services/fs.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyD1RrSZga3qhleSUf9uiweZWkyK7zZspic',
+  authDomain: 'cmpd-explorers-christmas.firebaseapp.com',
+  databaseURL: 'https://cmpd-explorers-christmas.firebaseio.com',
+  projectId: 'cmpd-explorers-christmas',
+  storageBucket: 'cmpd-explorers-christmas.appspot.com',
+  messagingSenderId: '575635321245'
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export default firebase.storage();
--- frontend/applications/nominations/src/services/household.ts
+++ frontend/applications/nominations/src/services/household.ts
@@ -1,4 +1,5 @@
 import db from './db';
+import fs from './fs';
 
 export function reviewHousehold(id, payload) {
   const { approved, reason, message } = payload;
@@ -40,6 +41,60 @@ export function submitNomination({ id }) {
     });
 }
 
-export function uploadAttachment({ id, file }) {
-  const formData = new FormData();
+export function uploadAttachment({ id, fileName, file }) {
+  const fileRef = fs.ref().child(`attachments/${id}/${fileName}`);
+  return fileRef.put(file);
+}
+
+export function deleteAttachment({ id, fileName }) {
+  const fileRef = fs.ref().child(`attachments/${id}/${fileName}`);
+  return fileRef.delete();
+}
+
+export type Household = {
+  id: string;
+  address: Address;
+  approved: boolean;
+  deleted: boolean;
+  dob: string;
+  draft: boolean;
+  email: string;
+  firstName: string;
+  gender: string;
+  last4ssn: string;
+  lastName: string;
+  middleName: string;
+  nominationEmailSent: boolean;
+  nominatorId: string;
+  phoneNumbers: Phone[];
+  preferredContactMethod: string;
+  race: string;
+  reviewed: boolean;
+  status: HouseholdStatus;
+};
+
+export type Address = {
+  city: string;
+  householdId: string;
+  state: string;
+  street: string;
+  street2: string;
+  type: string;
+  zip: string;
+};
+
+export type Phone = {
+  householdId: number;
+  number: string;
+  type: string;
+};
+
+export enum HouseholdStatus {
+  Drafted = 'DRAFTED',
+  Submitted = 'SUBMITTED',
+  Reviewed = 'REVIEWED',
+  ReadyToApprove = 'READY_TO_APPROVE',
+  Approved = 'APPROVED',
+  Declined = 'DECLINED',
+  Incomplete = 'INCOMPLETE'
 }
